393f5f9e7e1018d0471413e347cc93c7c1bfe4b6
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-31 13:29:38 +0200

    add User model

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Please try different email"],
+    match: [
+      /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+      "Please provide a valid email",
+    ],
+  },
+  role: {
+    type: String,
+    default: "user",
+    enum: ["user", "admin"],
+  },
+  password: {
+    type: String,
+    minlength: [6, "Password must be at least 6 characters"],
+    required: [true, "Please provide a password"],
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  blocked: {
+    type: Boolean,
+    default: false,
+  },
+});
+module.exports = mongoose.model("User", UserSchema);
