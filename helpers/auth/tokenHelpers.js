e7fd1d405bed9ec8a3cbdfbdcf3b3dde4c0a5c93
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-31 20:20:07 +0200

    add helper func getAccessTokenFromHeader modify getAccessToRoute

--- helpers/auth/tokenHelpers.js
+++ helpers/auth/tokenHelpers.js
@@ -26,4 +26,14 @@ const isTokenIncludedToHeaders = (req) => {
   );
 };
 
-module.exports = { sendJwtToClient, isTokenIncludedToHeaders };
+const getAccessTokenFromHeader = (req) => {
+  const authorization = req.headers.authorization;
+  const access_token = authorization.split(" ")[1];
+  return access_token;
+};
+
+module.exports = {
+  sendJwtToClient,
+  isTokenIncludedToHeaders,
+  getAccessTokenFromHeader,
+};
--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -1,13 +1,27 @@
 const CustomError = require("../../helpers/error/CustomError");
 const jwt = require("jsonwebtoken");
-const { isTokenIncludedToHeaders } = require("../../helpers/auth/tokenHelpers");
+const {
+  isTokenIncludedToHeaders,
+  getAccessTokenFromHeader,
+} = require("../../helpers/auth/tokenHelpers");
 
 const getAccessToRoute = (req, res, next) => {
+  const { JWT_SECRET_KEY } = process.env;
   if (!isTokenIncludedToHeaders(req)) {
     return next(
       new CustomError("You are not authorized to access this route", 401)
     );
   }
-  next();
+  const accessToken = getAccessTokenFromHeader(req);
+  jwt.verify(accessToken, JWT_SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return next(
+        new CustomError("You are not authorized to access this route", 401)
+      );
+    }
+
+    console.log(decoded);
+    next();
+  });
 };
 module.exports = { getAccessToRoute };
