b27df804f3571355f0ed44843900f9e0ac9c0ab9
Nihat Benli <nihatbenli.nb@gmail.com>
2021-08-31 20:22:21 +0200

    change token expire time

--- middlewares/auth/auth.js
+++ middlewares/auth/auth.js
@@ -19,7 +19,10 @@ const getAccessToRoute = (req, res, next) => {
         new CustomError("You are not authorized to access this route", 401)
       );
     }
-
+    req.user = {
+      id: decoded.id,
+      name: decoded.name,
+    };
     console.log(decoded);
     next();
   });
